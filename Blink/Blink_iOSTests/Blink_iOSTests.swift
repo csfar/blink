15f5b94fe407e68fc2dd6a8b735a2f7dbb6f0dc7
EdgSgroi <edgar.sgroi@gmail.com>
2020-07-08 17:07:54 -0300

    Add the Multipeer singleton and the basic multipeer logic in the BrainstormingViewModel and IntroViewModel

---
+++ Blink/Blink_iOSTests/Blink_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Blink_iOSTests.swift
+//  Blink_iOSTests
+//
+//  Created by Edgar Sgroi on 08/07/20.
+//  Copyright © 2020 Artur Carneiro. All rights reserved.
+//
+
+import XCTest
+@testable import Blink_iOS
+
+class Blink_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
